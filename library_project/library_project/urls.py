ec319504912342fa0670943558dc2e39db432241
metmirr <metin7087@gmail.com>
2019-12-01 18:07:11 +0300

    Add view/serializer class for book

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("books.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("books.urls")),
+    path("api/", include("api.urls")),
+]
