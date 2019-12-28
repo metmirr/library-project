c08fc3168266b2513b19bec5441c83ac625b2714
metmirr <metin7087@gmail.com>
2019-12-28 23:13:02 +0300

    Test publication date

--- library_project/books/tests.py
+++ library_project/books/tests.py
@@ -1,3 +1,11 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookMethodTests(TestCase):
+    def test_recent_pub(self):
+        futuredate = timezone.now().date() + datetime.timedelta(days=5)
+        future_pub = Book(publication_date=futuredate)
+        self.assertEqual(future_pub.recent_publication(), False)
