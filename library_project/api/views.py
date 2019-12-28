086b5c6aea932de1cf47e026d99714fd04800427
metmirr <metin7087@gmail.com>
2019-12-28 23:20:40 +0300

    Returns total number of books

--- library_project/api/views.py
+++ library_project/api/views.py
@@ -1,7 +1,15 @@
+from django.http import JsonResponse
 from rest_framework import generics
+from django.views import View
+from django.db.models import Count
 
 from books.models import Book
 from .serializers import BookSerializer
+
+
+def total_book(request):
+    count = Book.objects.aggregate(number_of_books=Count("title"))
+    return JsonResponse(count, safe=False)
 
 
 class BookAPIView(generics.ListAPIView):
