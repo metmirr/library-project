9621dcbf3a16e457b740ae99954496693168fd7c
metmirr <metin7087@gmail.com>
2019-12-29 21:32:22 +0300

    Add Publisher and Auhother model

--- library_project/books/models.py
+++ library_project/books/models.py
@@ -3,12 +3,34 @@ from django.utils import timezone
 import datetime
 
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    website = models.URLField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    subtitle = models.CharField(max_length=250)
-    author = models.CharField(max_length=250)
+    authors = models.ManyToManyField(Author, related_name="books")
     isbn = models.CharField(max_length=13)
-    publication_date = models.DateField(auto_now=True)
+    publication_date = models.DateField()
 
     def __str__(self):
         return self.title
+
