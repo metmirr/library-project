11842d37b3dcebd9b22bac602d0be80a232f97aa
metmirr <metin7087@gmail.com>
2020-01-03 23:19:05 +0300

    Register publisher model to admin site

--- library_project/books/admin.py
+++ library_project/books/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Publisher
 
 
 class BookAdminModel(admin.ModelAdmin):
-    list_display = ("title", "author", "isbn", "subtitle")
+    list_display = ("title", "isbn")
+
+
+class PublisherAdminModel(admin.ModelAdmin):
+    list_display = ("name", "website")
 
 
 admin.site.register(Book, BookAdminModel)
+admin.site.register(Publisher, PublisherAdminModel)
